ca854d2624b5dd62da0d020ea829ccc8864feff1
Swapnil Yasasvi <swapnilyasasvi2020@gmail.com>
2023-01-18 02:05:56 +0530

    Bug_last_two_alphabets_same_case,some_spaces_and_numerals_etc_not_present_in_ecah_line_output.txt

---
+++ Assignment1/P4/Assgn1_4_15.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+inputfile="./input4.txt"
+inputkey="kharagpur"
+while read line
+do
+    if [[ $line =~ $inputkey ]]
+    then
+        echo $line | sed 's/[a-zA-Z]/\L&/g' | sed 's/\([a-zA-Z]\)[^a-zA-Z]*\([a-zA-Z]\)/\U\1\L\2/g'  >> output.txt
+    else
+        if [[ $line != '' ]]
+        then
+            echo $line >> output.txt
+        fi
+    fi
+done < $inputfile
