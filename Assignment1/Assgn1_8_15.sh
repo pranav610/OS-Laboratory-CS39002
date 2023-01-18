b20fe8b7c26c7a6f48c44f2e4992997281fd8eca
sidvisw <sid1232100@gmail.com>
2023-01-19 05:19:58 +0530

    all done

--- Assignment1/Assgn1_8_15.sh
+++ Assignment1/Assgn1_8_15.sh
@@ -5,7 +5,7 @@ while getopts ":c:n:s:h" opt; do
   case $opt in
     c) flagc=true;category=$OPTARG;;
     n) flagn=true;name=$OPTARG;;
-    s) flags=true;column_name=$OPTARG;;
+    s) flags=true;[ "$OPTARG" = "date" ] && column_no=1;[ "$OPTARG" = "category" ] && column_no=2;[ "$OPTARG" = "amount" ] && column_no=3;[ "$OPTARG" = "name" ] && column_no=4;;
     h) echo "NAME\n\tExpense Tracker\n\nSYNOPSIS\n\tsh Assgn1_8_15.sh [OPTION]... ENTRY\n\nDESCRIPTION\n\tThis script tracks expenses by inserting new records into main.csv, and can also display total expenses by category or name, and sort the csv by a specified column.\n\n\t-c, --category\n\t\tDisplay total expenses by category.\n\n\t-n, --name\n\t\tDisplay total expenses by name.\n\n\t-s, --sort\n\t\tSort the csv by a specified column.\n\n\t-h, --help\n\t\tDisplay this help and exit.\n\nAUTHOR\n\tWritten by: Kulkarni Pranav Suryakant, Utsav Vinay Mehta, Swapnil Yasasvi, and Sidharth Vishwakarma.";exit;;
     \?) echo "Invalid option: -$OPTARG" >&2;exit;;
   esac
@@ -15,7 +15,7 @@ shift $((OPTIND-1))
 echo "$1,$2,$3,$4" >> main.csv
 
 header=$(head -1 main.csv)
-tail -n +2 main.csv | sort -t, -k $(head -1 main.csv | tr , '\n' | grep -n date | cut -f1 -d:) > temp.csv
+tail -n +2 main.csv | sort -t, -k 1.7n -k 1.4,1.5n -k 1.1,1.2n > temp.csv
 
 
 if [ "$flagc" = true ]; then
@@ -27,11 +27,10 @@ if [ "$flagn" = true ]; then
 fi
 
 if [ "$flags" = true ]; then
-  header=$(head -1 main.csv)
-  if [ "$column_name" = "amount" ]; then
-    tail -n +2 main.csv | sort -t, -k $(head -1 main.csv | tr , '\n' | grep -n $column_name | cut -f1 -d:) -n > temp.csv
+  if [ "$column_name" = 3 ]; then
+    tail -n +2 main.csv | sort -t, -k3 -n > temp.csv
   else
-    tail -n +2 main.csv | sort -t, -k $(head -1 main.csv | tr , '\n' | grep -n $column_name | cut -f1 -d:) > temp.csv
+    tail -n +2 main.csv | sort -t, -k$column_no > temp.csv
   fi
 fi
 
