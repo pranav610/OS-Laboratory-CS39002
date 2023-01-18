d8e96f60388b9414db5d56c878694972d12b3b09
pranav610 <pranavkulkarni610@gmail.com>
2023-01-19 01:57:39 +0530

    Q1.2 Done 24 WC

--- 1/Assgn1_2_15.sh
+++ 1/Assgn1_2_15.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 while IFS= read -r u; do
-    echo "$u" | grep -qP "^(?=[a-zA-Z])(?=.*[0-9])(?=.{4,19})" || echo "NO" && awk '{if(s+0==0 && index(tolower(var),tolower($1))){print"NO";s=1;}};END{if(s+0==0)print"YES";}' var="$u" fruits.txt 
+    if echo $u|grep -qP "^(?=[a-zA-Z])(?=.*[0-9])(?=.{4,19})";then
+        awk '{if(s+0==0 && index(tolower(var),tolower($1))){print"NO";s=1;}};END{if(s+0==0)print"YES";}' var="$u" fruits.txt
+    else 
+        echo "NO"
+    fi 
 done <usernames.txt>"$1"
