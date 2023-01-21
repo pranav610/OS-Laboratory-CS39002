5a579e4e0496d39413c57a5ea7ca82f10c275f27
Utsav Mehta <utsavmehta2002@yahoo.com>
2023-01-21 11:52:38 +0530

    Reduced word count in 1.9

--- 1.9/script.sh
+++ 1.9/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 awk '{print$2}' ./$1|sort -n|uniq -c|sort -k1rn -k2|awk '{print$2"\t"$1}'
-awk '{print$1}' ./$1|awk '{if(!($0 in seen)){seen[$0];i=i+1}else{print$0;i=i-1}}; END{print i+0}'
+awk '{if(!($1 in seen)){seen[$1];i=i+1}else{print$1;i=i-1}}; END{print i+0}' ./$1
