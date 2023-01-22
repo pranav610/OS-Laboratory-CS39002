979d8373edc62b10343f3d9af2bf96efa9d47f06
Swapnil Yasasvi <swapnilyasasvi2020@gmail.com>
2023-01-22 19:08:14 +0530

    P1_final

--- Assignment1/P4/Assgn1_4_15.sh
+++ Assignment1/P4/Assgn1_4_15.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 while IFS= read -r line
 do
-    if echo "$line" | grep -qw "$2"
+    if echo "$line" | grep -qw "$2";
     then
         echo "$line" | sed 's/[a-zA-Z]/\U&/g' | sed 's/\([a-zA-Z]\)\([^a-zA-Z]*\)\([a-zA-Z]\)/\U\1\2\L\3/g'
     else
