dcb41d083fda21d93837b64b8b512c13a65e4cd9
pranav610 <pranavkulkarni610@gmail.com>
2023-01-25 10:51:06 +0530

    Q1.3 Complete

--- 1/Assgn1_2_15.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while IFS= read -r u; do
-    if echo $u|grep -qP "(?=^[a-zA-Z][a-zA-Z0-9]*$)(?=.*[0-9])(?=^.{5,20}$)";then
-        awk '{if(!(s+0) && index(tolower(var),tolower($1))){print"NO";s=1;}};END{if(!(s+0))print"YES";}' var="$u" fruits.txt
-    else 
-        echo "NO"
-    fi 
-done <usernames.txt>"$1"
