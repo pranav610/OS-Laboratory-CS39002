0bf7adfb764046c613ac97c10c26fc8d638258f7
Utsav Mehta <utsavmehta2002@yahoo.com>
2023-02-25 20:08:17 +0530

    Some changes to make optimize work.

--- Assignment3/consumer.cpp
+++ Assignment3/consumer.cpp
@@ -11,6 +11,7 @@ int main(int argc, char const *argv[])
 {
     int consumer_process_count = atoi(argv[1]);
     string file_name = string(argv[1]);
+    bool optimized = (argc > 2);
 
     key_t unique_key = ftok("./shared_memory_file", 15);
     srand(time(0));
@@ -30,104 +31,109 @@ int main(int argc, char const *argv[])
 
     char *str = (char *)shmat(shmid, (void *)0, 0);
 
-    // cout << "Graph: " << str << endl;
-    stringstream ss;
-
-    ss << str;
-    int node_count = 0, node1, node2;
-    string word;
-
-    while (!ss.eof())
+    while (1)
     {
-        ss >> word;
-        if (stringstream(word) >> node1)
-            node_count = max(node_count, node1);
-        word = "";
-    }
+        stringstream ss;
 
-    vector<vector<int>> adj_list(++node_count);
+        ss << str;
+        int node_count = 0, node1, node2;
+        string word;
 
-    stringstream ss2;
-    ss2 << str;
-
-    node1 = -1;
-    while (!ss2.eof())
-    {
-        ss2 >> word;
-        if (node1 == -1)
+        while (!ss.eof())
         {
-            stringstream(word) >> node1;
+            ss >> word;
+            if (stringstream(word) >> node1)
+                node_count = max(node_count, node1);
             word = "";
         }
-        else
+
+        vector<vector<int>> adj_list(++node_count);
+
+        stringstream ss2;
+        ss2 << str;
+
+        node1 = -1;
+        while (!ss2.eof())
         {
-            stringstream(word) >> node2;
-            adj_list[node1].push_back(node2);
-            adj_list[node2].push_back(node1);
-            word = "";
-            node1 = -1;
-        }
-    }
-
-    int node_set_start = (node_count / 10) * consumer_process_count;
-    int node_set_end = (node_count / 10) * (consumer_process_count + 1);
-
-    FILE *fp = fopen(("./" + file_name + ".txt").c_str(), "w");
-
-    for (int i = node_set_start; i < node_set_end; i++)
-    {
-        int dist[node_count];
-        int par[node_count];
-
-        for (int j = 0; j < node_count; j++)
-        {
-            dist[j] = INT_MAX;
-            par[j] = -1;
-        }
-
-        set<pair<int, int>> s;
-        s.insert({0, i});
-        dist[i] = 0;
-
-        while (!s.empty())
-        {
-            int nn = s.begin()->second;
-            s.erase(s.begin());
-
-            for (auto &j : adj_list[nn])
+            ss2 >> word;
+            if (node1 == -1)
             {
-                if (dist[j] > dist[nn] + 1)
+                stringstream(word) >> node1;
+                word = "";
+            }
+            else
+            {
+                stringstream(word) >> node2;
+                adj_list[node1].push_back(node2);
+                adj_list[node2].push_back(node1);
+                word = "";
+                node1 = -1;
+            }
+        }
+
+        int node_set_start = (node_count / 10) * consumer_process_count;
+        int node_set_end = (node_count / 10) * (consumer_process_count + 1);
+
+        FILE *fp = fopen(("./" + file_name + ".txt").c_str(), "w");
+
+        for (int i = node_set_start; i < node_set_end; i++)
+        {
+            int dist[node_count];
+            int par[node_count];
+
+            for (int j = 0; j < node_count; j++)
+            {
+                dist[j] = INT_MAX;
+                par[j] = -1;
+            }
+
+            set<pair<int, int>> s;
+            s.insert({0, i});
+            dist[i] = 0;
+
+            while (!s.empty())
+            {
+                int nn = s.begin()->second;
+                s.erase(s.begin());
+
+                for (auto &j : adj_list[nn])
                 {
-                    par[j] = nn;
-                    s.erase({dist[j], j});
-                    dist[j] = dist[nn] + 1;
-                    s.insert({dist[j], j});
+                    if (dist[j] > dist[nn] + 1)
+                    {
+                        par[j] = nn;
+                        s.erase({dist[j], j});
+                        dist[j] = dist[nn] + 1;
+                        s.insert({dist[j], j});
+                    }
                 }
             }
-        }
 
-        for(int j = 0; j < node_count; j++)
-        {
-            if(i == j || dist[j] == INT_MAX)
-                continue;
-            
-            string path = "";
-            int temp = j;
-            vector<int> path_nodes;
-            while(temp != i)
+            for (int j = 0; j < node_count; j++)
             {
-                path_nodes.push_back(temp);
-                temp = par[temp];
+                if (i == j || dist[j] == INT_MAX)
+                    continue;
+
+                string path = "";
+                int temp = j;
+                vector<int> path_nodes;
+                while (temp != i)
+                {
+                    path_nodes.push_back(temp);
+                    temp = par[temp];
+                }
+                path_nodes.push_back(i);
+
+                reverse(path_nodes.begin(), path_nodes.end());
+
+                for (auto &k : path_nodes)
+                    path += to_string(k) + " ";
+
+                fprintf(fp, "%s\n", path.c_str());
             }
-            path_nodes.push_back(i);
-            
-            reverse(path_nodes.begin(), path_nodes.end());
-
-            for(auto &k : path_nodes)
-                path += to_string(k) + " ";
-
-            fprintf(fp, "%s\n", path.c_str());
         }
+
+        sleep(30);
+        break;  
     }
 
     // sleep(10);
