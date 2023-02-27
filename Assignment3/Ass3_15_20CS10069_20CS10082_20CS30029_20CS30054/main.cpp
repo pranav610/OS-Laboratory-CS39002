f76f14789e8da77482113d66d65070a434dcf92c
pranav610 <pranavkulkarni610@gmail.com>
2023-02-27 14:56:08 +0530

    A commit

---
+++ Assignment3/Ass3_15_20CS10069_20CS10082_20CS30029_20CS30054/main.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <sys/ipc.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sys/shm.h>
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+
+#define CONSUMER_COUNT 10
+
+int main()
+{
+    key_t unique_key = ftok("./shared_memory_file", 15);
+
+    if (unique_key == -1)
+    {
+        perror("ftok");
+        exit(EXIT_FAILURE);
+    }
+
+    // int a = SHMMIN;
+
+    size_t sz = 100000000;
+
+    int shmid = shmget(unique_key, sz, IPC_CREAT | 0666);
+    printf("shmid: %d\n", shmid);
+    // shmctl(shmid, IPC_RMID, NULL);
+    unique_key = ftok("./shared_memory_file", 15);
+    // shmid = shmget(unique_key, sz, IPC_CREAT | 0666);
+    if (shmid == -1)
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+
+    char *str = (char *)shmat(shmid, (void *)0, 0);
+
+    string graph = "";
+    FILE *fp = fopen("facebook_combined.txt", "r");
+
+
+    while (1)
+    {
+        char c = fgetc(fp);
+        if (c == EOF)
+            break;
+
+        graph += c;
+    }
+
+
+    memcpy(str, graph.c_str(), graph.length());
+    pid_t producer_pid = fork();
+    
+    if (producer_pid == 0)
+    {
+        // producer process here
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {   
+        set<pid_t> consumer_pids;
+        for(int i=0; i<CONSUMER_COUNT; ++i)
+        {   
+            pid_t consumer_pid = fork();
+            if (consumer_pid == 0)
+            {
+                // consumer process here
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                consumer_pids.insert(consumer_pid);
+            }
+        }
+        for(auto pid: consumer_pids)
+            waitpid(pid, NULL, 0);
+        waitpid(producer_pid, NULL, 0);
+    }
+
+    shmdt(str);
+    shmctl(shmid, IPC_RMID, NULL);
+    return 0;
+}
