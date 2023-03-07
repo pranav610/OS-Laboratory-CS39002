f461412ae05e42bd86d27e0f2a93557a0f01eff2
Utsav Mehta <utsavmehta2002@yahoo.com>
2023-03-07 12:08:05 +0530

    Final submission added.

--- Assignment3/consumer.cpp
+++ Assignment3/consumer.cpp
@@ -4,17 +4,192 @@
 #include <sys/shm.h>
 #include <iostream>
 #include <sstream>
+#include <time.h>
+#include <chrono>
 
 using namespace std;
 
 #define CONSUMER_COUNT 10
+#define CONS_SLEEP 30
+unordered_map<int, vector<int>> source_dist, source_par;
 
 set<int> nodes_assigned;
 
+int consumer_process_count;
+
+string file_name;
+
+int new_node_count;
+
+void djkstra(vector<vector<int>> &adj_list)
+{
+    // auto start = chrono::high_resolution_clock::now();
+    FILE *fp = fopen(("./" + file_name + ".txt").c_str(), "w");
+    int node_count = adj_list.size();
+    for (auto &i : nodes_assigned)
+    {
+        vector<int> &dist = source_dist[i];
+        vector<int> &par = source_par[i];
+        dist.resize(node_count);
+        par.resize(node_count);
+
+        for (int j = 0; j < node_count; j++)
+        {
+            dist[j] = INT_MAX;
+            par[j] = -1;
+        }
+
+        set<pair<int, int>> s;
+        s.insert({0, i});
+        dist[i] = 0;
+
+        while (!s.empty())
+        {
+            int nn = s.begin()->second;
+            s.erase(s.begin());
+
+            for (auto &j : adj_list[nn])
+            {
+                if (dist[j] > dist[nn] + 1)
+                {
+                    par[j] = nn;
+                    s.erase({dist[j], j});
+                    dist[j] = dist[nn] + 1;
+                    s.insert({dist[j], j});
+                }
+            }
+        }
+
+        for (int j = 0; j < node_count; j++)
+        {
+            if (i == j || dist[j] == INT_MAX)
+                continue;
+
+            string path = "";
+            int temp = j;
+            vector<int> path_nodes;
+            while (temp != i)
+            {
+                path_nodes.push_back(temp);
+                temp = par[temp];
+            }
+            path_nodes.push_back(i);
+
+            reverse(path_nodes.begin(), path_nodes.end());
+
+            for (auto &k : path_nodes)
+                path += to_string(k) + " ";
+
+            fprintf(fp, "%s\n", path.c_str());
+        }
+    }
+
+    fflush(fp);
+    fclose(fp);
+}
+
+void djkstra_optimized(vector<vector<int>> &adj_list)
+{
+    FILE *fp = fopen(("./" + file_name + ".txt").c_str(), "w");
+    int node_count = adj_list.size();
+
+    for (int i = node_count - new_node_count; i < node_count; i++)
+    {
+        vector<int> dist, par;
+
+        dist.resize(node_count);
+        par.resize(node_count);
+
+        for (int j = 0; j < node_count; j++)
+        {
+            dist[j] = INT_MAX;
+            par[j] = -1;
+        }
+
+        queue<int> q;
+        q.push(i);
+        dist[i] = 0;
+
+        while (!q.empty())
+        {
+            int nn = q.front();
+            q.pop();
+
+            for (auto &j : adj_list[nn])
+            {
+                if (dist[j] > dist[nn] + 1)
+                {
+                    par[j] = nn;
+                    dist[j] = dist[nn] + 1;
+                    q.push(j);
+                }
+            }
+        }
+
+        if (i % 10 == consumer_process_count)
+            source_dist[i] = dist, source_par[i] = par;
+
+        for (auto &node_assigned : nodes_assigned)
+        {
+            source_dist[node_assigned].resize(node_count, INT_MAX);
+            source_par[node_assigned].resize(node_count, -1);
+            for (int j = 0; j < node_count; j++)
+            {
+                if (source_dist[node_assigned][j] > dist[node_assigned] + dist[j])
+                {
+                    source_dist[node_assigned][j] = dist[node_assigned] + dist[j];
+                    source_par[node_assigned][j] = par[j];
+                }
+            }
+
+            if (i == node_assigned)
+                continue;
+
+            int nn = node_assigned, parent = par[nn];
+            while (parent != i)
+            {
+                nn = parent;
+                parent = par[nn];
+            }
+            source_par[node_assigned][i] = nn;
+        }
+    }
+    for (auto &i : nodes_assigned)
+    {
+        for (int j = 0; j < node_count; j++)
+        {
+            vector<int> &dist = source_dist[i];
+            vector<int> &par = source_par[i];
+
+            if (i == j || dist[j] == INT_MAX)
+                continue;
+
+            string path = "";
+            int temp = j;
+            vector<int> path_nodes;
+            while (temp != i)
+            {
+                path_nodes.push_back(temp);
+                temp = par[temp];
+            }
+            path_nodes.push_back(i);
+
+            reverse(path_nodes.begin(), path_nodes.end());
+
+            for (auto &k : path_nodes)
+                path += to_string(k) + " ";
+
+            fprintf(fp, "%s\n", path.c_str());
+        }
+    }
+    fflush(fp);
+    fclose(fp);
+}
+
 int main(int argc, char const *argv[])
 {
-    int consumer_process_count = atoi(argv[1]);
-    string file_name = string(argv[1]);
+    consumer_process_count = atoi(argv[1]);
+    file_name = string(argv[1]);
     bool optimized = (argc > 2);
 
     key_t unique_key = ftok("./shared_memory_file", 15);
@@ -27,7 +202,7 @@ int main(int argc, char const *argv[])
     }
 
     char *str;
-
+    int prev_node_count = 0;
     for (int _ = 0;; _++)
     {
         int shmid = shmget(unique_key, 0, 0666);
@@ -76,7 +251,8 @@ int main(int argc, char const *argv[])
                 node1 = -1;
             }
         }
-
+        new_node_count = node_count - prev_node_count;
+        bool is_subsequent_iteration = !nodes_assigned.empty();
         int i = (nodes_assigned.empty() ? consumer_process_count : *nodes_assigned.rbegin());
         while (i < node_count)
         {
@@ -84,73 +260,24 @@ int main(int argc, char const *argv[])
             i += CONSUMER_COUNT;
         }
 
-        FILE *fp = fopen(("./" + file_name + ".txt").c_str(), "w");
-
-        for (auto &i : nodes_assigned)
+        if (is_subsequent_iteration)
         {
-            int dist[node_count];
-            int par[node_count];
-
-            for (int j = 0; j < node_count; j++)
+            if (optimized)
             {
-                dist[j] = INT_MAX;
-                par[j] = -1;
+                djkstra_optimized(adj_list);
             }
-
-            set<pair<int, int>> s;
-            s.insert({0, i});
-            dist[i] = 0;
-
-            while (!s.empty())
+            else
             {
-                int nn = s.begin()->second;
-                s.erase(s.begin());
-
-                for (auto &j : adj_list[nn])
-                {
-                    if (dist[j] > dist[nn] + 1)
-                    {
-                        par[j] = nn;
-                        s.erase({dist[j], j});
-                        dist[j] = dist[nn] + 1;
-                        s.insert({dist[j], j});
-                    }
-                }
-            }
-
-            for (int j = 0; j < node_count; j++)
-            {
-                if (i == j || dist[j] == INT_MAX)
-                    continue;
-
-                string path = "";
-                int temp = j;
-                vector<int> path_nodes;
-                while (temp != i)
-                {
-                    path_nodes.push_back(temp);
-                    temp = par[temp];
-                }
-                path_nodes.push_back(i);
-
-                reverse(path_nodes.begin(), path_nodes.end());
-
-                for (auto &k : path_nodes)
-                    path += to_string(k) + " ";
-
-                fprintf(fp, "%s\n", path.c_str());
+                djkstra(adj_list);
             }
         }
+        else
+        {
+            djkstra(adj_list);
+        }
 
-        fflush(fp);
-        fclose(fp);
-
-        if (consumer_process_count == 1)
-            printf("cons-check-1\n");
-
-        sleep(30);
-        if (_ == 4)
-            break;
+        prev_node_count = node_count;
+        sleep(CONS_SLEEP);
     }
 
     shmdt(str);
