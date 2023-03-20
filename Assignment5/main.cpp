be60a426f2fe9fa99237d5c2210179166279ef21
pranav610 <pranavkulkarni610@gmail.com>
2023-03-20 15:19:18 +0530

    Asgn5 soma changes

--- Assignment5/main.cpp
+++ Assignment5/main.cpp
@@ -6,23 +6,31 @@ vector<pthread_t> cleaning_tids;
 
 int main()
 {
-    cout << "Enter number of rooms: "; cin >> Y;
-    cout << "Enter number of guests: "; cin >> N;
+    cout << "Enter number of rooms: "; cin >> N;
+    cout << "Enter number of guests: "; cin >> Y;
     cout << "Enter number of cleaners: "; cin >> X;
 
-    // create threads for guests and cleaners
-    guest_tids.resize(N);
-    cleaning_tids.resize(X);             
+    if(Y > N && N > X && X > 1)
+    {
+        // create threads for guests and cleaners
+        guest_tids.resize(Y);
+        cleaning_tids.resize(X);             
 
-    for(int i=0; i<N; i++)
-        pthread_create(&guest_tids[i], NULL, guest, (void*)i);
-    for(int i=0; i<X; i++)
-        pthread_create(&cleaning_tids[i], NULL, cleaner, (void*)i);
-    
-    // wait for all threads to finish
-    for(int i=0; i<N; i++)
-        pthread_join(guest_tids[i], NULL);
-    for(int i=0; i<X; i++)
-        pthread_join(cleaning_tids[i], NULL);
+        for(int i=0; i<Y; i++)
+            pthread_create(&guest_tids[i], NULL, guest, (void*)i);
+        for(int i=0; i<X; i++)
+            pthread_create(&cleaning_tids[i], NULL, cleaner, (void*)i);
+        
+        // wait for all threads to finish
+        for(int i=0; i<Y; i++)
+            pthread_join(guest_tids[i], NULL);
+        for(int i=0; i<X; i++)
+            pthread_join(cleaning_tids[i], NULL);
+    }
+    else
+    {
+        cout << "Invalid input" << endl;
+        return 0;
+    }
     
 }
--- Assignment5/main.hpp
+++ Assignment5/main.hpp
@@ -12,3 +12,20 @@ using namespace std;
 void *guest(void *arg);
 void *cleaner(void *arg);
 
+class Room
+{
+    public:
+        int room_id;
+        int guest_id;
+        bool is_clean;
+        bool is_occupied;
+        Room(int room_id)
+        {
+            this->room_id = room_id;
+            this->guest_id = -1;
+            this->is_clean = true;
+            this->is_occupied = false;
+        }
+};
+
+
