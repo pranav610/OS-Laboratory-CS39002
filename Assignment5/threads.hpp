51945aed20d87993735e0b411d7c91639930b2e3
Utsav Mehta <utsavmehta2002@yahoo.com>
2023-03-20 18:48:12 +0530

    Added thread for cleaners.

--- Assignment5/threads.hpp
+++ Assignment5/threads.hpp
@@ -1,6 +1,12 @@
+
+#include <bits/stdc++.h>
+#include "data_structures.hpp"
+#include <semaphore.h>
+
 #ifndef THREADS_HPP
 #define THREADS_HPP
 
+using namespace std;
 void *guest(void *arg);
 void *cleaner(void *arg);
 
